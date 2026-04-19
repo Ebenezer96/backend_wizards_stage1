bbdb3149e506f9eab86f28ea83ed899bba3314ae
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2026-04-19 09:17:19 +0100

    Ensure correct API routes

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProfileCollectionView, ProfileDetailView
 
 urlpatterns = [
-    path("profiles", ProfileCollectionView.as_view()),
-    path("profiles/<uuid:profile_id>", ProfileDetailView.as_view()),
+    path("profiles", ProfileCollectionView.as_view(), name="profile-collection"),
+    path("profiles/<int:profile_id>", ProfileDetailView.as_view(), name="profile-detail"),
 ]
